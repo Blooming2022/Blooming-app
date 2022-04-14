8e67836c3089c7049af1f1095517a670089a0ab5
YoonJeon <yoonjeon0413@gmail.com>
2022-04-14 21:35:18 +0900

    Initial

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Initial</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+});
+
+export default App;
