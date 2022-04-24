5872a8f83d2aef0b717ad3f4aa2cc374f51f4ecd
YoonJeon <yoonjeon0413@gmail.com>
2022-04-24 15:12:43 +0900

    Add splash screen

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
 } from 'react-native';
 import MainTabNavigator from './src/navigation/MainTabNavigator';
+import SplashScreen from 'react-native-splash-screen';
 
-const App = () => {
+const App = () =>
+{
+  useEffect(() => {
+    try {
+      setTimeout(() => {
+        SplashScreen.hide();
+      }, 2000);
+    } catch (e) {
+      console.log(e.message);
+    }
+  } );
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <MainTabNavigator></MainTabNavigator>
