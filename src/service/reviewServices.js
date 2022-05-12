583320184b2e89978819d44f22098a91f11e0279
a419802 <a419802@users.noreply.github.com>
2022-05-12 11:13:28 +0900

    Update reviewServices.js
    export 방식 수정,
    alert()로 에러 메세지 띄우는 코드에서 console.log(e.message)로 변경

--- src/service/reviewServices.js
+++ src/service/reviewServices.js
@@ -4,18 +4,18 @@ import auth from '@react-native-firebase/auth';
 const usersCollection = firestore().collection('users');
 
 // 후기 생성
-export const createRev = async (revData) => {
+const createRev = async (revData) => {
   try {
     const user = auth().currentUser;
     return await usersCollection.doc(user.uid).collection('revList').doc().set(revData);
   } catch (e) {
-    alert("정상적으로 처리되지 않았습니다. 다시 시도해주세요.");
+    console.log(e.message);
     return -1;
   }
 }
 
 // 후기 조회
-export const readRev = async () => {
+const readRev = async () => {
   try {
     const user = auth().currentUser;
     const data = await usersCollection.doc(user.uid).collection('revList').get();
@@ -24,29 +24,35 @@ export const readRev = async () => {
     return ret;
   } catch (error) {
     console.log(error.message);
-    alert("정상적으로 처리되지 않았습니다. 다시 시도해주세요.");
     return -1;
   }
 }
 
 // 후기 수정
-export const updateRev = async (revID, revData) => {
+const updateRev = async (revID, revData) => {
   try {
     const user = auth().currentUser;
     return await usersCollection.doc(user.uid).collection('revList').doc(revID).update(revData);
   } catch (e) {
-    alert("정상적으로 처리되지 않았습니다. 다시 시도해주세요.");
+    console.log(e.message);
     return -1;
   }
 }
 
 // 후기 삭제
-export const deleteRev = async (revID) => {
+const deleteRev = async (revID) => {
   try {
     const user = auth().currentUser;
     return await usersCollection.doc(user.uid).collection('revList').doc(revID).delete();
   } catch (e) {
-    alert("정상적으로 처리되지 않았습니다. 다시 시도해주세요.");
+    console.log(e.message);
     return -1;
   }
+}
+
+export {
+  createRev,
+  readRev,
+  updateRev,
+  deleteRev
 }
