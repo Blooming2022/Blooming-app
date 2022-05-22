2de21d43948bac2cd47a1c2e1a7533ceb5623512
a419802 <a419802@users.noreply.github.com>
2022-05-22 15:48:40 +0900

    Squashed commit of the following:
    commit d709d5c01f4c0ce237ebe3c5ac1b970dfa1af2fb
    Merge: 9ecc734 d06801b
    Author: YoonJeon <51810552+bella0413@users.noreply.github.com>
    Date:   Thu May 12 14:04:48 2022 +0900
    
        Merge pull request #4 from Blooming2022/back/authServices
    
        authServices: 유저 인증 정보를 파이어베이스와 연동
    
    commit 9ecc734c01541dbceb3ad3c8329feb08627ac61c
    Merge: 544d40b 4ae1864
    Author: SAYNO-hub <61473348+SAYNO-hub@users.noreply.github.com>
    Date:   Thu May 12 01:18:35 2022 +0900
    
        Merge pull request #6 from Blooming2022/feature/nickname
    
        로그인, 닉네임 생성 화면 구현
    
    commit 544d40b162d02e23c590fd4e247fe4d4ba6e7e8d
    Merge: 3215194 57b0c53
    Author: NaKyoung-Kim <78343969+NaKyoung-Kim@users.noreply.github.com>
    Date:   Wed May 11 20:27:55 2022 +0900
    
        Merge pull request #3 from Blooming2022/back/initialSetting
    
        파이어베이스 연동 기본 설정
    
    commit 4ae186462dfe12d4641dc7be8d0c40236c55575a
    Author: YoonJeon <yoonjeon0413@gmail.com>
    Date:   Wed May 11 18:55:57 2022 +0900
    
        Make Nickname feature
    
    commit 2ac3c443d3ae447646adec4e5acd255f9c11b0d3
    Author: YoonJeon <yoonjeon0413@gmail.com>
    Date:   Wed May 11 17:00:53 2022 +0900
    
        Make Login Screen
    
    commit 3215194fe5bb60fe7361d60fb8ef287da8ce657c
    Author: YoonJeon <51810552+bella0413@users.noreply.github.com>
    Date:   Wed May 11 16:05:32 2022 +0900
    
        Update .gitignore

---
+++ src/containers/login/Login.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native';
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleBox}>
+        <Text style={styles.title}>Blooming</Text>
+        <View style={styles.flexStart}>
+          <Text style={styles.subTitle}>나라는 꽃</Text>
+          <Text style={styles.subTitle}>피우기</Text>
+        </View>
+      </View>
+      <Image
+        source={require('../../assets/images/logo.png')}
+        style={styles.logo}
+        resizeMode="contain"></Image>
+      <TouchableOpacity style={[styles.button, styles.btn1]}>
+        <Text style={styles.btnText1}>게스트로 로그인</Text>
+      </TouchableOpacity>
+      <TouchableOpacity style={[styles.button, styles.btn2]}>
+        <View style={styles.googleLogin}>
+          <Image
+            source={require('../../assets/images/logo_googleg_48dp.png')}></Image>
+          <Text style={styles.btnText2}>Google로 로그인</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+  },
+  titleBox: {
+    marginTop: 30,
+    marginBottom: 60,
+    width: '80%',
+  },
+  flexStart: {
+    justifyContent: 'flex-start',
+  },
+  title: {
+    fontSize: 58,
+    fontWeight: 'bold',
+    color: '#242424',
+  },
+  subTitle: {
+    fontSize: 26,
+    color: '#969696',
+  },
+  logo: {
+    width: '100%',
+    height: 200,
+    marginBottom: 70,
+  },
+  button: {
+    width: '80%',
+    height: 67,
+    borderRadius: 35,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 17,
+  },
+  btn1: {
+    backgroundColor: '#242424',
+  },
+  btn2: {
+    backgroundColor: '#ffffff',
+    borderColor: '#CBCBCB',
+    borderWidth: 1,
+  },
+  googleLogin: {
+    flexDirection: 'row',
+    width: 140,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  btnText1: {
+    fontSize: 16,
+    color: '#ffffff',
+  },
+  btnText2: {
+    fontSize: 16,
+    color: '#242424',
+  },
+});
+
+export default Login;
