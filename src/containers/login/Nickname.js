6273d5c5a37d28df7c54f2afc089083d0949b4cc
a419802 <a419802@users.noreply.github.com>
2022-05-23 17:41:43 +0900

    Squashed commit of the following:
    commit caefa176053c225a12083e4022eab70f5ccc4bf8
    Merge: 866233c ed848f4
    Author: YoonJeon <51810552+bella0413@users.noreply.github.com>
    Date:   Fri May 20 18:59:54 2022 +0900
    
        Merge pull request #11 from Blooming2022/fix/issue
    
        android package, app 이름 변경 및 새로운 firebase와 연동
    
    commit ed848f4b6c83affd0a672a75c4c5cd4cec1194e1
    Author: YoonJeon <yoonjeon0413@gmail.com>
    Date:   Fri May 20 16:29:05 2022 +0900
    
        Rename package. Change and link firebase
    
    commit 866233c845895cb54b3fb1899d96ffbe74f9c61e
    Merge: 57f8d13 2d21bdf
    Author: YoonJeon <51810552+bella0413@users.noreply.github.com>
    Date:   Thu May 12 14:04:48 2022 +0900
    
        Merge pull request #4 from Blooming2022/back/authServices
    
        authServices: 유저 인증 정보를 파이어베이스와 연동
    
    commit 2d21bdf6c392c01f16ec5cc5f45d483bc7e8b480
    Author: a419802 <a419802@users.noreply.github.com>
    Date:   Thu May 12 11:06:04 2022 +0900
    
        Update authServices.js
    
        export 방식 수정,
        alert() 로 처리했던 에러 메세지를 console.log(e.message)로 수정,
        deleteAccount() 내의 잘못된 경로 수정
    
    commit 57f8d13aadb1e2e16507e99c37d8b80c6d0ab557
    Merge: 3317f9d 8b99044
    Author: SAYNO-hub <61473348+SAYNO-hub@users.noreply.github.com>
    Date:   Thu May 12 01:18:35 2022 +0900
    
        Merge pull request #6 from Blooming2022/feature/nickname
    
        로그인, 닉네임 생성 화면 구현
    
    commit 3317f9d9a10003b7151719b7d1e4fe6323059b45
    Merge: 28c27e5 3706b14
    Author: NaKyoung-Kim <78343969+NaKyoung-Kim@users.noreply.github.com>
    Date:   Wed May 11 20:27:55 2022 +0900
    
        Merge pull request #3 from Blooming2022/back/initialSetting
    
        파이어베이스 연동 기본 설정
    
    commit 8b990447e1b2d46ec3ba066365a3ae45d5519210
    Author: YoonJeon <yoonjeon0413@gmail.com>
    Date:   Wed May 11 18:55:57 2022 +0900
    
        Make Nickname feature
    
    commit 0d39ef88fde1989137823505ea79b562b3e141e7
    Author: YoonJeon <yoonjeon0413@gmail.com>
    Date:   Wed May 11 17:00:53 2022 +0900
    
        Make Login Screen
    
    commit 26768b19e412f04fc6526c80e177ef77e38912c1
    Author: a419802 <a419802@users.noreply.github.com>
    Date:   Wed May 11 16:26:36 2022 +0900
    
        Update authServices.js
    
    commit 9f16cfdc06abf93b8f0682d6eafa625088899b50
    Author: a419802 <a419802@users.noreply.github.com>
    Date:   Wed May 11 16:24:43 2022 +0900
    
        Update authServices.js
    
    commit 0b42bb3702b7d2b5c66a1c9fe611bf83bf4f167f
    Author: a419802 <a419802@users.noreply.github.com>
    Date:   Wed May 11 16:01:27 2022 +0900
    
        Create authServices.js
    
    commit 3706b141a4905d832dcb65fa78eb4ac785ba8d6b
    Author: a419802 <a419802@users.noreply.github.com>
    Date:   Tue May 10 15:32:52 2022 +0900
    
        add Initial Setting

---
+++ src/containers/login/Nickname.js
@@ -0,0 +1,88 @@
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Platform,
+  Keyboard,
+  KeyboardAvoidingView,
+  ScrollView,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import SaveButton from '../../components/Button/SaveButton';
+import NicknameInput from './components/NicknameInput';
+
+const Nickname = () => {
+  const [isExistName, setIsExistName] = useState(false);
+  const [buttonDisabled, setButtonDisable] = useState(true);
+  const [nickname, setNickname] = useState('');
+  // 임시로 만든 닉네임 중복 검사 함수
+  const checkIsExistingNickname = nickname => {
+    const list = ['가', '나', '다'];
+    return list.includes(nickname);
+  };
+
+  return (
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : null}
+      style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView style={styles.content}>
+          <View style={styles.header}>
+            <TouchableOpacity>
+              <Image
+                style={styles.cancelImage}
+                source={require('../../assets/images/x.png')}></Image>
+            </TouchableOpacity>
+          </View>
+          <Text style={styles.title}>사용하실 닉네임을 입력해주세요.</Text>
+          <NicknameInput
+            nickname={nickname}
+            setNickname={setNickname}
+            setButtonDisable={setButtonDisable}
+            isExistName={isExistName}
+            setIsExistName={setIsExistName}></NicknameInput>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+      <SaveButton
+        title="확인"
+        value={nickname}
+        setValueIsNotValid={setIsExistName}
+        checkFunction={checkIsExistingNickname}
+        buttonDisabled={buttonDisabled}
+        setButtonDisable={setButtonDisable}
+        // navigation={navigation}
+      ></SaveButton>
+    </KeyboardAvoidingView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  content: {
+    paddingHorizontal: 20,
+  },
+  header: {
+    alignItems: 'flex-start',
+    marginTop: 20,
+    marginBottom: 50,
+  },
+  cancelImage: {
+    resizeMode: 'contain',
+    width: 24,
+    height: 24,
+  },
+  title: {
+    fontSize: 20,
+    color: '#242424',
+    marginBottom: 50,
+    fontWeight: 'bold',
+  },
+});
+
+export default Nickname;
