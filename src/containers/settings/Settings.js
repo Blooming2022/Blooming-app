391ea1496b843ad7e2a1af3eea8e709dc9368e12
YoonJeon <yoonjeon0413@gmail.com>
2022-06-21 16:25:28 +0900

    Change myPage tab to settings tab

---
+++ src/containers/settings/Settings.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Settings = () => {
+  return (
+    <View>
+      <Text>settig</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#F8F8F8',
+    flex: 1,
+  },
+});
+
+export default Settings;
